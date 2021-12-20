3024d12116600722d69e68bd9372aeaffb0626e4
ramarh1 <88624677+ramarh1@users.noreply.github.com>
2021-12-20 16:02:47 -0500

    Updated R script with delivery 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,17 @@
 library(dplyr)
+#Delivery 1
 dataframe <- read.csv("MechaCar_mpg.csv")
 dataframe
 linear_regression <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data = dataframe)
 summary(linear_regression)
+
+#Delivery 2
+mecha_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- mecha_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
+
+lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep',)
+
+
+
 
