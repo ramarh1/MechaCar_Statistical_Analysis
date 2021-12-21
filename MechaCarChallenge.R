fa680895500a613a4581e1a507934f01a6f65628
ramarh1 <88624677+ramarh1@users.noreply.github.com>
2021-12-20 20:36:53 -0500

    Updated R script with delivery 3 t-tests

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,5 +13,20 @@ lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=me
 Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep',)
 
 
+#Delivery 3 t-tests
+#Total t-test
+t.test(mecha_coil$PSI, mu=1500)
+
+#Lot 1 Table t-test
+lot1 <- subset(mecha_coil, Manufacturing_Lot=="Lot1")
+t.test(lot1$PSI,mu=1500)
+
+#Lot 2 Table t-test
+lot2 <- subset(mecha_coil, Manufacturing_Lot=="Lot2")
+t.test(lot2$PSI,mu=1500)
+
+#Lot 3 Table t-test
+lot3 <- subset(mecha_coil, Manufacturing_Lot=="Lot3")
+t.test(lot3$PSI,mu=1500)
 
 
